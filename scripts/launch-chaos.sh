78463ed1e729995d1a7ad689042553a3f6774f64
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-03-28 15:58:03 +0530

    Update launch-chaos.sh

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -e 
+curl -L https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0/hce-api-linux-amd64 -o hce-api
 
-curl --location 'https://app.harness.io/gateway/chaos/manager/api/query?accountIdentifier=cTU1lRSWS2SSRV9phKvuOA' \
---header 'x-api-key: pat.cTU1lRSWS2SSRV9phKvuOA.6422a0973c72fb2b03ee36b8.RVJsGxLiq9Zt2AYyNp7D' \
---header 'Content-Type: application/json' \
---data '{"query":"mutation RunChaosExperiment(\n  $workflowID: String!,\n  $identifiers: IdentifiersRequest!\n) {\n  runChaosExperiment(\n    workflowID: $workflowID,\n    identifiers: $identifiers\n  ) {\n    notifyID\n  }\n}","variables":{"workflowID":"9bd2855f-b822-464c-9906-0f9ebe824cc6","identifiers":{"orgIdentifier":"default","accountIdentifier":"cTU1lRSWS2SSRV9phKvuOA","projectIdentifier":"ChaosTestinProd2"}}}'
+chmod +x hce-api
+
+./hce-api generate --api launch-experiment --hce-endpoint=${HCE_ENDPOINT} \
+--project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
+--access-key ${ACCESS_KEY} --access-id ${ACCESS_ID} --file-name hce-api.sh
