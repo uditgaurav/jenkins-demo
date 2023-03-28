5bdb06b32b8df42ec4c22027265f95931c4e5026
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-03-28 19:39:43 +0530

    Update monitor-chaos.sh

--- scripts/monitor-chaos.sh
+++ scripts/monitor-chaos.sh
@@ -3,29 +3,6 @@ curl -L https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0/h
 
 chmod +x hce-api
 
-./hce-api generate --api monitor-experiment --hce-endpoint=${HCE_ENDPOINT} \
---project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
---access-key ${ACCESS_KEY} --access-id ${ACCESS_ID} --file-name hce-api.sh
-chmod +x hce-api.sh
-
-DELAY=2
-RETRY=150
-START=0
-fileName="hce-api.sh"
-i=0
-while [ "$i" -le $RETRY ]; do
-    i=$(( i + 1 ))
-    echo
-    res=$(sh $fileName)
-    echo
-    if [ "$res" = "Succeeded" ]; then
-        echo "Experiment completed, CurrentState: $res"
-        exit 0
-    fi
-    sleep $DELAY
-    echo "Waiting for experiment completion... CurrentState: $res, retries left: $(($RETRY-$i))"
-done
-
-echo "[Error]: Timeout the workflows is not completed with DELAY: $DELAY and RETRY: $RETRY, CurrentState: $res"
-
-exit 1
+./hce-api-saas generate --api monitor-experiment --account-id=${ACCOUNT_ID} \
+--project-id ${PROJECT_ID} --workflow-run-id ${WORKFLOW_RUN_ID} \
+--api-key ${API_KEY} --file-name hce-api.sh --timeout="300"
