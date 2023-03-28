5020f1e07dabe7942af5cef0e59e0ac773f2b103
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-03-28 12:41:44 +0530

    Update launch-chaos.sh

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl --location 'https://app.harness.io/gateway/chaos/manager/api/query?accountIdentifier=cTU1lRSWS2SSRV9phKvuOA' \
---header 'x-api-key: pat.cTU1lRSWS2SSRV9phKvuOA.64228498bfaff60df53f385b.cATd1pTv7UwOwS1XVvYj' \
+
+curl --location 'https://app.harness.io/gateway/chaos/manager/api/query?accountIdentifier=${ACCOUNT_ID}' \
+--header 'x-api-key: ${API_KEY}' \
 --header 'Content-Type: application/json' \
---data '{"query":"mutation RunChaosExperiment(\n  $workflowID: String!,\n  $identifiers: IdentifiersRequest!\n) {\n  runChaosExperiment(\n    workflowID: $workflowID,\n    identifiers: $identifiers\n  ) {\n    notifyID\n  }\n}","variables":{"workflowID":"9bd2855f-b822-464c-9906-0f9ebe824cc6","identifiers":{"orgIdentifier":"default","accountIdentifier":"cTU1lRSWS2SSRV9phKvuOA","projectIdentifier":"ChaosTestinProd2"}}}'
+--data '{"query":"mutation RunChaosExperiment(\n  $workflowID: String!,\n  $identifiers: IdentifiersRequest!\n) {\n  runChaosExperiment(\n    workflowID: $workflowID,\n    identifiers: $identifiers\n  ) {\n    notifyID\n  }\n}","variables":{"workflowID":"${WORKFLOW_ID}","identifiers":{"orgIdentifier":"default","accountIdentifier":"${ACCOUNT_ID}","projectIdentifier":"${PROJECT_ID}"}}}'
