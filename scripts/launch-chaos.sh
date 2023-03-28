d2b2d69af0dff207d0ad4ef56820acb92e3bde95
Karthik Satchitanand <karthik.s@harness.io>
2023-03-28 13:40:26 +0530

    Update launch-chaos.sh

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -2,6 +2,6 @@
 set -e 
 
 curl --location 'https://app.harness.io/gateway/chaos/manager/api/query?accountIdentifier=cTU1lRSWS2SSRV9phKvuOA' \
---header 'x-api-key: pat.cTU1lRSWS2SSRV9phKvuOA.64228498bfaff60df53f385b.cATd1pTv7UwOwS1XVvYj' \
+--header 'x-api-key: pat.cTU1lRSWS2SSRV9phKvuOA.6422a0973c72fb2b03ee36b8.RVJsGxLiq9Zt2AYyNp7D' \
 --header 'Content-Type: application/json' \
 --data '{"query":"mutation RunChaosExperiment(\n  $workflowID: String!,\n  $identifiers: IdentifiersRequest!\n) {\n  runChaosExperiment(\n    workflowID: $workflowID,\n    identifiers: $identifiers\n  ) {\n    notifyID\n  }\n}","variables":{"workflowID":"9bd2855f-b822-464c-9906-0f9ebe824cc6","identifiers":{"orgIdentifier":"default","accountIdentifier":"cTU1lRSWS2SSRV9phKvuOA","projectIdentifier":"ChaosTestinProd2"}}}'
