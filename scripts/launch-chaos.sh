8681bdb81ab2a8cb22051d38d1274bb2a111189b
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-03-28 16:00:32 +0530

    Update launch-chaos.sh

--- scripts/launch-chaos.sh
+++ scripts/launch-chaos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl -L https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0/hce-api-linux-amd64 -o hce-api
+curl -L https://github.com/uditgaurav/hce-api-template/releases/download/0.1.0-saas/hce-api-linux-amd64 -o hce-api
 
 chmod +x hce-api
 
-./hce-api generate --api launch-experiment --hce-endpoint=${HCE_ENDPOINT} \
+./hce-api-saas generate --api launch-experiment --account-id=${ACCOUNT_ID} \
 --project-id ${PROJECT_ID} --workflow-id ${WORKFLOW_ID} \
---access-key ${ACCESS_KEY} --access-id ${ACCESS_ID} --file-name hce-api.sh
+--api-key ${API_KEY} --file-name hce-api.sh
